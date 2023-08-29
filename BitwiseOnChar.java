446b890ecb6642185851e4e4d8dcd501ea9741ab
Niranjan-GopaL <niranjangopalpvl@gmail.com>
2023-08-30 01:10:34 +0530

    YOU CAN BITWISE ON CHAR damn

---
+++ BitwiseOnChar.java
@@ -0,0 +1,25 @@
+/**
+ * BitwiseOnChar
+ */
+public class BitwiseOnChar {
+
+    public static void main(String[] args) {
+        char character = 'a';
+        // ASCII of a => 97
+
+        System.out.println(97 & 1);
+        System.out.println(character & 1);
+
+
+         
+        /* 
+
+        Output
+
+        1
+        1
+
+        */
+    }
+    
+}
